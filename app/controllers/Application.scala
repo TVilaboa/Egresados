6eee2a0df36c10000131cb9137059db83081719b
matiasjlopez <matias.lopez@ing.austral.edu.ar>
2016-08-29 21:46:34 -0300

    "LaNacionUrlGenerator" class created. Test in controllers.Application

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,8 +1,21 @@
 package controllers
 
+import generators.LaNacionUrlGenerator
 import play.api.mvc._
 
+import scala.collection.mutable.ListBuffer
+
 class Application extends Controller {
+
+  //Test La Nacion URL Generator, se los dejo por si querian ver.
+  val generator: LaNacionUrlGenerator = new LaNacionUrlGenerator()
+  val links: ListBuffer[String] = generator.searchLaNacionUrl("lopez gabeiras")
+  println("*********************************")
+  for(link <- links) {
+    println(link)
+  }
+  println("*********************************")
+
 
   def index = Action {
     Ok(views.html.index.render())
