d46e7af7dea0c5977196b52c3b42443d7b0bde58
Franco Testori <ftestori@keyneticdigital.com>
2017-08-03 19:38:45 -0300

    Refactor rutas de Search de noticias y LinkedInProfile

--- app/controllers/ElCronistaNewsController.scala
+++ app/controllers/ElCronistaNewsController.scala
@@ -2,73 +2,59 @@ package controllers
 
 import actions.SecureAction
 import com.google.inject.Inject
-import generators.{ElCronistaUrlGenerator, ElCronistaUrlGeneratorObject}
-import models.{Graduate, News}
+import generators.ElCronistaUrlGeneratorObject
+import models.{News, Prospect}
 import play.api.mvc.{Action, Controller}
 import scrapers.ElCronistaScraper
-import services.{ElCronistaNewsService, GraduateService}
+import services.{ElCronistaNewsService, ProspectService}
 
 import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
+import scala.concurrent.Await
 
 /**
   * Created by matias on 21/09/2016.
   */
-class ElCronistaNewsController @Inject()(newsElCronistaService: ElCronistaNewsService, graduateService: GraduateService, secureAction: SecureAction) extends Controller{
+class ElCronistaNewsController @Inject()(newsElCronistaService: ElCronistaNewsService,
+                                         prospectService: ProspectService,
+                                         scraper: ElCronistaScraper,
+                                         secureAction: SecureAction) extends Controller{
 
-  def saveNews(id : String) = secureAction {
-    val generator: ElCronistaUrlGenerator = new ElCronistaUrlGenerator()
-    var graduate : Graduate = Await.result(graduateService.find(id),Duration.Inf)
-    val fullName : Option[String]= Option(graduate.firstName + " " +graduate.lastName)
-    val links = ElCronistaUrlGeneratorObject.search(fullName,Option("Universidad Austral"))
-    val scraper: ElCronistaScraper = new ElCronistaScraper()
-    var news: List[News] = List[News]()
-    var element: Option[News] = None
-    for(link <- links) {
-      if (!link.equals(null)) {
-        element = scraper.getArticleData(link,fullName,0)
-      }
-      if (element.isDefined) {
-        newsElCronistaService.save(element.get)
-        news = element.get :: news
-      }
+  def search(id: String) = Action{
+    val prospect : Prospect = Await.result(prospectService.find(id), Duration.Inf)
+
+    runSearch(prospect)
+
+    Redirect(routes.ProspectController.show(id))
+  }
+
+  def searchAll = Action{
+    val prospects : Seq[Prospect] = Await.result(prospectService.all(), Duration.Inf)
+
+    prospects.foreach(x=>runSearch(x))
+
+    Redirect(routes.ProspectController.index(""))
+  }
+
+  def runSearch(prospect: Prospect) : Unit = {
+    val name : Option[String] = Option(prospect.getFullName)
+    val links : Seq[String] = ElCronistaUrlGeneratorObject.search(name, Option(prospect.institution.name))
+
+    val news : Seq[News] = links.map{x=> scraper.getArticleData(x,name,0)}.filter(_.isDefined).map(_.get)
+
+    if(news.nonEmpty){
+      val activeNews: List[String] = prospect.infobaeNews.map(_.url)
+      val difference : List[News] = news.filter(n=> !activeNews.contains(n.url)).toList
+      difference.map(newsElCronistaService.save)
+
+      val all : List[News] = prospect.infobaeNews ::: difference
+      Await.result(prospectService.update(prospect.copy(infobaeNews = all)), Duration.Inf)
     }
-    graduate = graduate.copy(elCronistaNews = news)
-    Await.result(graduateService.update(graduate),Duration.Inf)
-
-    Redirect("/profile/" + graduate._id)
-
   }
 
   def deleteNews(id:String) = Action {
     //Get graduate from DB.
     val news : News = Await.result(newsElCronistaService.find(id),Duration.Inf)
     Await.result(newsElCronistaService.drop(news), Duration.Inf)
-    Redirect("/")
-  }
-
-
-  def saveAllElCronistaNews = Action {
-    val scraper : ElCronistaScraper = new ElCronistaScraper()
-    val all : Future[Seq[Graduate]] = graduateService.all()
-    val graduates : Seq[Graduate] = Await.result(all,Duration.Inf)
-    graduates.foreach{grad : Graduate =>
-      var newsList: List[News] = List[News]()
-      val fullName : Option[String]= Option(grad.firstName + " " +grad.lastName)
-      val links = ElCronistaUrlGeneratorObject.search(fullName,Option("Universidad Austral"))
-      var element: Option[News] = null
-      links.foreach{link : String =>
-
-        element = scraper.getArticleData(link,fullName,0)
-        if (element.isDefined) {
-          newsElCronistaService.save(element.get)
-          newsList = element.get :: newsList
-        }
-      }
-      val graduate = grad.copy(elCronistaNews = newsList)
-      Await.result(graduateService.update(graduate),Duration.Inf)
-    }
-
     Redirect("/")
   }
 }
--- app/controllers/LaNacionNewsController.scala
+++ app/controllers/LaNacionNewsController.scala
@@ -1,47 +1,54 @@
 package controllers
 
-import java.sql.Timestamp
-import java.util.Date
-
 import actions.SecureAction
 import com.google.inject.Inject
-import generators.{InfobaeUrlGeneratorObject, LaNacionUrlGenerator, LaNacionUrlGeneratorObject}
-import models.{Graduate, News}
+import generators.LaNacionUrlGeneratorObject
+import models.{News, Prospect}
 import play.api.mvc.{Action, Controller}
-import scrapers.{InfobaeScraper, LaNacionScraper}
-import services.{GraduateService, LaNacionNewsService}
+import scrapers.LaNacionScraper
+import services.{LaNacionNewsService, ProspectService}
 
-import scala.collection.mutable.ListBuffer
-import scala.concurrent.{Await, Future}
+import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
 /**
   * Created by matias on 21/09/2016.
   */
-class LaNacionNewsController @Inject() (newsLaNacionService: LaNacionNewsService,graduateService: GraduateService, secureAction: SecureAction) extends Controller{
+class LaNacionNewsController @Inject() (newsLaNacionService: LaNacionNewsService,
+                                        prospectService: ProspectService,
+                                        scraper: LaNacionScraper,
+                                        secureAction: SecureAction) extends Controller{
 
-  def saveNews(id : String) = secureAction {
-    val generator: LaNacionUrlGenerator = new LaNacionUrlGenerator()
-    var graduate : Graduate = Await.result(graduateService.find(id),Duration.Inf)
-    val fullName : Option[String]= Option(graduate.firstName + " " +graduate.lastName)
-    val links = LaNacionUrlGeneratorObject.search(fullName,Option("Universidad Austral"))
-    val scraper: LaNacionScraper = new LaNacionScraper()
-    var news: List[News] = List[News]()
-    var element: Option[News] = None
-    for(link <- links) {
-      if (!link.equals(null)) {
-        element = scraper.getArticleData(link,fullName,0)
-      }
-      if (element.isDefined) {
-        newsLaNacionService.save(element.get)
-        news = element.get :: news
-      }
+  def search(id: String) = Action{
+    val prospect : Prospect = Await.result(prospectService.find(id), Duration.Inf)
+
+    runSearch(prospect)
+
+    Redirect(routes.ProspectController.show(id))
+  }
+
+  def searchAll = Action{
+    val prospects : Seq[Prospect] = Await.result(prospectService.all(), Duration.Inf)
+
+    prospects.foreach(x=>runSearch(x))
+
+    Redirect(routes.ProspectController.index(""))
+  }
+
+  def runSearch(prospect: Prospect) : Unit = {
+    val name : Option[String] = Option(prospect.getFullName)
+    val links : Seq[String] = LaNacionUrlGeneratorObject.search(name, Option(prospect.institution.name))
+
+    val news : Seq[News] = links.map{x=> scraper.getArticleData(x,name,0)}.filter(_.isDefined).map(_.get)
+
+    if(news.nonEmpty){
+      val activeNews: List[String] = prospect.infobaeNews.map(_.url)
+      val difference : List[News] = news.filter(n=> !activeNews.contains(n.url)).toList
+      difference.map(newsLaNacionService.save)
+
+      val all : List[News] = prospect.infobaeNews ::: difference
+      Await.result(prospectService.update(prospect.copy(infobaeNews = all)), Duration.Inf)
     }
-    graduate = graduate.copy(laNacionNews = news)
-    Await.result(graduateService.update(graduate),Duration.Inf)
-
-    Redirect("/profile/" + graduate._id)
-
   }
 
   def deleteNews(id:String) = Action {
@@ -51,28 +58,4 @@ class LaNacionNewsController @Inject() (newsLaNacionService: LaNacionNewsService
     Redirect("/")
   }
 
-
-  def saveAllLaNacionNews = Action {
-    val scraper : LaNacionScraper = new LaNacionScraper()
-    val all : Future[Seq[Graduate]] = graduateService.all()
-    val graduates : Seq[Graduate] = Await.result(all,Duration.Inf)
-    graduates.foreach{grad : Graduate =>
-      var newsList: List[News] = List[News]()
-      val fullName : Option[String]= Option(grad.firstName + " " +grad.lastName)
-      val links = LaNacionUrlGeneratorObject.search(fullName,Option("Universidad Austral"))
-      var element: Option[News] = null
-      links.foreach{link : String =>
-
-        element = scraper.getArticleData(link,fullName,0)
-        if (element.isDefined) {
-          newsLaNacionService.save(element.get)
-          newsList = element.get :: newsList
-        }
-      }
-      val graduate = grad.copy(laNacionNews = newsList)
-      Await.result(graduateService.update(graduate),Duration.Inf)
-    }
-
-    Redirect("/")
-  }
 }
