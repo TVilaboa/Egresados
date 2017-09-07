62f791431be30e1168d1cc2371b3c1944116547c
TVilaboa <tvilaboa@hotmail.com>
2017-09-07 12:12:28 -0300

    Fix Test

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,9 +1,8 @@
+import org.junit.runner._
 import org.specs2.mutable._
 import org.specs2.runner._
-import org.junit.runner._
-
-import play.api.test._
 import play.api.test.Helpers._
+import play.api.test._
 
 @RunWith(classOf[JUnitRunner])
 class ApplicationSpec extends Specification {
@@ -11,7 +10,7 @@ class ApplicationSpec extends Specification {
   "Application" should {
 
     "send 404 on a bad request" in new WithApplication {
-      route(FakeRequest(GET, "/boum")).get.value must beNone
+      route(FakeRequest(GET, "/boum")).get.value must equalTo(404)
     }
 
     "check the service is alive" in new WithApplication {
